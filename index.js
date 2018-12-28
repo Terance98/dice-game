4fdcbbced8aabe40252ecf36a59e503633568ec8
Terance98 <thomasterance98@gmail.com>
2018-12-28 13:08:37 +0530

    This is the initial commit and the project is completed

---
+++ index.js
@@ -0,0 +1,16 @@
+var randomNumber1 = Math.random();
+randomNumber1 = Math.floor((randomNumber1 * 6)) + 1;
+
+var randomNumber2 = Math.random();
+randomNumber2 = Math.floor((randomNumber2 * 6)) + 1;
+
+document.querySelector('.img1').setAttribute('src', 'images/dice' + randomNumber1 + '.png');
+document.querySelector('.img2').setAttribute('src', 'images/dice' + randomNumber2 + '.png');
+
+if (randomNumber1 > randomNumber2) {
+  document.querySelector('h1').innerHTML = 'Player 1 WINS !!!🚩';
+}else if (randomNumber1 === randomNumber2) {
+  document.querySelector('h1').innerHTML = 'DRAW!!!';
+}else {
+  document.querySelector('h1').innerHTML = 'Player 2 WINS !!!🚩';
+}
